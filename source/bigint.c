5d189470b1cd6a380097c985a1d6213ae2cda1ba
chiabert <chiabert@eurecom.fr>
2019-03-17 17:32:40 +0100

    simo changes: added sum and sub + #if else conditions in bigint.h

--- source/bigint.c
+++ source/bigint.c
@@ -9,7 +9,7 @@ int eq(bigint_t first, bigint_t second){
     }
     for(int i=0; i<NUMB_SIZE; i++){
         if(first.numb[i]!=second.numb[i])
-           return 0; 
+           return 0;
     }
     return 1;
 };
@@ -21,12 +21,12 @@ int df(bigint_t first, bigint_t second){
     }
     for(int i=0; i<NUMB_SIZE; i++){
         if(first.numb[i]!=second.numb[i])
-           return 1; 
+           return 1;
     }
     return 0;
 };
 
-// Return 1 if strictly gt, else 0; -1 if not aligned 
+// Return 1 if strictly gt, else 0; -1 if not aligned
 int gt(bigint_t first, bigint_t second){
     if (first.pos != second.pos){
         return -1;
@@ -40,7 +40,7 @@ int gt(bigint_t first, bigint_t second){
     return 0;
 };
 
-// Return 1 if ge, else 0; -1 if not aligned 
+// Return 1 if ge, else 0; -1 if not aligned
 int ge(bigint_t first, bigint_t second){
     if (first.pos != second.pos){
         return -1;
@@ -55,7 +55,7 @@ int ge(bigint_t first, bigint_t second){
 };
 
 
-// Return 1 if strictly lt, else 0; -1 if not aligned 
+// Return 1 if strictly lt, else 0; -1 if not aligned
 int lt(bigint_t first, bigint_t second){
     if (first.pos != second.pos){
         return -1;
@@ -69,7 +69,7 @@ int lt(bigint_t first, bigint_t second){
     return 0;
 };
 
-// Return 1 if le, else 0; -1 if not aligned 
+// Return 1 if le, else 0; -1 if not aligned
 int le(bigint_t first, bigint_t second){
     if (first.pos != second.pos){
             return -1;
@@ -122,4 +122,54 @@ int lsl(bigint_t *a, int pl){
 
     a->numb[0] <<= pl;
     return 1;
+}
+
+// Sum, gets data on NUMB_SIZE -1  and returns data on NUMB_SIZE
+bint_t sum(bint_t a, bint_t b){
+
+  bint_t data_res;
+
+  uint32_t carry = 0;
+  uint32_t i;
+
+  for (i = 0; i < NUMB_SIZE-1 ; i++) {
+    data_res.numb[i] = a.numb[i] + b.numb[i] + carry;
+    printf("stage %d sum = %x\n", i, data_res.numb[i]);
+    if (data_res.numb[i] < a.numb[i]){
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+  }
+
+  data_res.pos = INT_SIZE + carry*VAR_SIZE;
+  data_res.numb[i] = carry;
+  printf("and additional digits pos = %d, last = %x\n",data_res.pos,data_res.numb[i]);
+  return data_res;
+}
+
+// SUB, gets data on NUMB_SIZE -1  and returns data on NUMB_SIZE
+bint_t sub(bint_t a, bint_t b){
+
+  bint_t data_res;
+
+  uint32_t carry = 1;
+  uint32_t i, k, j;
+
+  for (i = 0; i < NUMB_SIZE-1 ; i++) {
+    b.numb[i] = b.numb[i]^UMAX;
+    data_res.numb[i] = a.numb[i] + b.numb[i] + carry;
+    printf("stage %d sub = %x", i, data_res.numb[i]);
+    if (data_res.numb[i] <= a.numb[i]){
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    printf(" with next carry %x\n",carry);
+  }
+
+  data_res.pos = INT_SIZE + !carry*VAR_SIZE;
+  data_res.numb[i] = 0-!carry;
+  printf("and additional digits pos = %d, last = %x\n",data_res.pos,data_res.numb[i]);
+  return data_res;
 }
