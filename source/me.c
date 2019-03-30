0e399cb940c6036af84a7452083fd63b5e4eab76
roggero <roggero@eurecom.fr>
2019-03-30 11:58:09 +0100

    ME_big initialized

--- source/me.c
+++ source/me.c
@@ -7,8 +7,8 @@
 /* 	e  = exponent
 	m  = message
 	n  = modulus
-	c0 = prcomputed value of: (1<<nb) % n
-	s0 = prcomputed value of: m*(1<<nb) % n
+	c0 = precomputed value of: (1<<nb) % n
+	s0 = precomputed value of: m*(1<<nb) % n
 	nb = number of bit
 */
 int ME(int e, int m, int n, int c0, int s0, int nb){
@@ -24,3 +24,23 @@ int ME(int e, int m, int n, int c0, int s0, int nb){
 	c = MM(c,1,n,nb);
 	return c;
 };
+
+
+bigint_t ME_big(bigint_t *e, bigint_t *m, bigint_t *n, bigint_t *c0, bigint_t *s0, int nb){
+    bigint_t c = *c0;
+    bigint_t s = *s0;
+    bigint_t mask = init(ONE);
+    bigint_t zero = init(ZERO);
+    bigint_t one = init(ONE);
+    bigint_t bit_and;
+
+    for(int i=0; i < nb; i++){
+        bit_and = and(&mask, e);
+        if(df(&zero, &bit_and)){
+            c = MM_big(&c, &s, n, nb);
+        }
+        s = MM_big(&s, &s, n, nb);
+        mask = lsl(&mask,1);
+    }
+    c = MM_big(&c, &one, n, nb);
+}
