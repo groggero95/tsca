3d18af6fe5db90d57b97656124b0b3fbc5ad4bc6
chiabert <chiabert@eurecom.fr>
2019-03-30 11:42:10 +0100

    forse forse va forse

--- test/mod_mul.py
+++ test/mod_mul.py
@@ -13,10 +13,10 @@ def padhex(m, nb=32):
     return '0x' + hex(m)[2:].zfill(int(math.ceil(nb/32)*8))
 
 
-def mm_test(sel=0, nbit=128, testnum=10000, over=1):
+def mm_test(sel=0, nbit=128, testnum=1000, over=1):
     """Perform the needed tests, taking as arguments the operation (MM, ME), number of bits, number of tests, scaling coefficient for struct bits """
-    pad = over*nbit
-    nbit_mod = 8
+    pad = 128 + 32
+    nbit_mod = 128
     path = './main'
     operation = ['mm', 'me']
     toterr = 0
@@ -25,11 +25,13 @@ def mm_test(sel=0, nbit=128, testnum=10000, over=1):
         a = random.getrandbits(nbit)
         b = random.getrandbits(nbit)
         n = random.getrandbits(nbit_mod) | 1
+        # n = (1<<nbit)-1
         c = subprocess.run([path, operation[sel], padhex(a, pad), padhex(
             b, pad), padhex(n, pad)], stdout=subprocess.PIPE)
 
+        r = int(gmpy2.invert(1 << nbit, n))
         if sel == 0:
-            res_t = (a*b*(int(gmpy2.invert(1 << pad, n)))) % n
+            res_t = (a*b*r) % n
         elif sel == 1:
             res_t = (a >> b) & ((1 << (nbit+32))-1)
 
@@ -38,16 +40,23 @@ def mm_test(sel=0, nbit=128, testnum=10000, over=1):
         # print("\n")
         # print(res_m)
         # print(padhex(res_t,nbit+32))
-        flag = (res_t == int(res_m, 16)) or (res_t == (int(res_m, 16))-n)
+        flag = (res_t == int(res_m, 16)%n)
         if not flag:
-            print("\n")
-            print(padhex(a, pad))
-            print(padhex(b, pad))
-            print(padhex(n, pad))
-            print(res_m)
-            print(padhex(res_t, pad+32))
-            print("\n")
+            # print("\n")
+            # print(padhex(a, pad))
+            # print(padhex(b, pad))
+            # print(padhex(n, pad))
+            # print(res_m)
+            # print(padhex(res_t, pad))
+            # print("\n")
             toterr = toterr + 1
+        print("\n")
+        print(padhex(a, pad))
+        print(padhex(b, pad))
+        print(padhex(n, pad))
+        print(res_m)
+        print(padhex(res_t, pad))
+        print("\n")
         sys.stdout.write(
             "\rTest " + operation[sel] + ": " + str(i+1) + "/" + str(testnum))
 
