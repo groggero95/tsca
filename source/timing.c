6d191761a62a56660e3346417b233c6d1ab79c27
Fausto Chiatante <fausto.chiatante@gmail.com>
2019-04-02 16:37:44 +0200

    Added parameter to testing script to decide between MM and ME

--- source/timing.c
+++ source/timing.c
@@ -4,11 +4,12 @@
 #include "bigint.h"
 #include "time_meas.h"
 #include "me.h"
+#include "mm.h"
 
 
 int main(int argc, char **argv){
 
-	int nb;
+	int nb, mode;
 	bigint_t a, b, n, res, k0;
 	time_stats_t *timer = (time_stats_t*) malloc(sizeof(time_stats_t));
 
@@ -16,13 +17,21 @@ int main(int argc, char **argv){
     b = init(argv[2]); // plain text
     n = init(argv[3]); // modulus
     k0 = init(argv[4]); // (1<<2nb) % n
+	mode = atoi(argv[5]);	// ME (0) or MM (1)
     nb = INT_SIZE+2;
 
     reset_meas(timer);
 	for (int i = 0; i < 10; ++i)
 	{
 		start_meas(timer);
-	    res = ME_big(a,b,n,k0,nb);
+		switch (mode){
+	    	case 0:
+				res = ME_big(a,b,n,k0,nb);
+				break;
+			case 1:
+				res = MM_big(a,b,n,nb);
+				break;
+		}
 	    stop_meas(timer);
 	}
 
--- test/timing.py
+++ test/timing.py
@@ -14,7 +14,7 @@ def padhex(m, nb=32):
     return '0x' + hex(m)[2:].zfill(int(math.ceil(nb/32)*8))
 
 
-def mm_test(nbit=128, testnum=1000):
+def mm_test(nbit=128, testnum=1000, mode=0):
     """Perform the needed tests, taking as arguments the operation (MM, ME), number of bits, number of tests, scaling coefficient for struct bits """
     pad = 128 + 32
     nbit_mod = 128
@@ -31,7 +31,7 @@ def mm_test(nbit=128, testnum=1000):
 #        print(padhex(a, pad),padhex(b, pad),padhex(n, pad),padhex(k0, pad))
 
         c = subprocess.run([path, padhex(a, pad), padhex(
-            b, pad), padhex(n, pad), padhex(k0, pad)], stdout=subprocess.PIPE)
+            b, pad), padhex(n, pad), padhex(k0, pad), str(mode)], stdout=subprocess.PIPE)
 
         res.append(int(c.stdout.decode('utf-8').strip('\n'),10))
     return res
@@ -44,11 +44,13 @@ if __name__ == '__main__':
         time = mm_test(int(sys.argv[1], 10))
     elif len(sys.argv) == 3:
         time = mm_test(int(sys.argv[1], 10), int(sys.argv[2], 10))
-    fp = open('data.txt','w')
-    for data in time:
-    	fp.write(str(data)+ '\n')
-    fp.close()
+    elif len(sys.argv) == 4:
+        time = mm_test(int(sys.argv[1], 10), int(sys.argv[2], 10), int(sys.argv[3], 10))
+    #fp = open('data.txt','w')
+    #for data in time:
+    #	fp.write(str(data)+ '\n')
+    #fp.close()
     plt.hist(time,bins=100)
-    plt.ylabel("Feequency")
+    plt.ylabel("Frequency")
     plt.xlabel("Clock cycles")
     plt.show()
