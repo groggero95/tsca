1715ba003af5f0bb91291c9fcf6b6d6535acfda2
Giulio Roggero <julrogg95@gmail.com>
2019-04-29 15:02:38 +0200

    Not working

--- attack/attack.py
+++ attack/attack.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import os, sys
+import numpy
+import scipy.stats as stat
 # def me_step(s, c )
 
 class guess_test():
@@ -11,8 +13,6 @@ class guess_test():
 		self.t = 0
 		self.c = (1<<nb)%n
 		self.s = (plain*(1<<nb)) % n
-
-
 
 
 
@@ -46,9 +46,9 @@ def mm_estimate(a,b,n,nb):
 
 	return estimate
 
-def me_step(c, s, e, n, step):
-	mask = 1 << step
-	if (e & mask):
+def me_step(c, s, e_bit, n):
+	# mask = 1 << step
+	if (e_bit):
 		c = mm(c,s,n,nb)
 	s = mm(s,s,n,nb)
 
@@ -72,8 +72,7 @@ def me(e,m,n,nb):
 		if (e & mask):
 			c = mm(c,s,n,nb)
 		s = mm(s,s,n,nb)
-		if (c>n or s>n):
-			print("errore overflow")
+
 		mask = mask << 1
 		# print(str(x) + ' ' + padhex(c,160) + ' ' + padhex(s,160))
 	c = mm(c,1,n,nb)
@@ -84,16 +83,18 @@ def read_plain(n, nb=130, file_msg='PLAIN.BIN', file_time='TIME.BIN', length_msg
 	f_msg = open(file_msg,"rb")
 	messages = list()
 	f_time = open(file_time, "rb")
+	T_arr = list()
 	while True:
 		read_msg = f_msg.read(length_msg)
 		read_time = f_time.read(length_time)
 		if not ((read_msg) and (read_time)):
 			break
 		msg = guess_test(int.from_bytes(read_msg, byteorder='little',signed=False), int.from_bytes(read_time, byteorder='little',signed=False), n, nb)
+		T_arr.append(msg.T)
 		messages.append(msg)
 	f_time.close()
 	f_msg.close()
-	return messages
+	return messages, T_arr
 
 
 
@@ -107,35 +108,53 @@ if __name__ == '__main__':
 	m       = 0x000000000882beb97c2eb4916c19d16f3
 	k0 	    = 0x08354f24c98cfac7a6ec8719a1b11ba4f
 	nb 		= 130
+	# Final to revert the key, as we start from LSB
+	private_key_bit = '0000101100101001011110010111001010010101011101110011101111011100000000100011100111011001010110110110100110101010110010100000000001'[::-1]
+	public_key_bit = 
 
 	#private exponent to be found
-	e = 0
+	e_guess = list()
 
 	#control
 	step = 0
 	nmess = 0
+	tau_corr = 0.3
+	tau_miss = 0.1
 
 	# c = (1<<nb)%n
 	# s = (m*(1<<nb)) % n
-	messages=read_plain(n=n, nb=nb)
+	messages, T_arr = read_plain(n=n, nb=nb)
+
+	messages[0] = m
 
 	#print(padhex(c,160))
 	#print(padhex(s,160))
 
-	for step in range(nb):
+	for step, bit in zip(range(nb), key_bit):
 		#calculate timing estimate based on guessing 1 for the current exponent bit
-		t = mm_estimate(c,s,n,nb)
-		for nmess, test in enumerate(messages):
+		t_arr = list()
+		for test in messages:
 			test.t = mm_estimate(test.c, test.s, n, nb)
+			t_arr.append(test.t)
 
-		#calculate pearson correlation coefficient
-
+		# Calculate Pearson correlation coefficient
+		pcc = stat.pearsonr(T_arr, t_arr)
+		# Chiatant's Th : the PCC, if a correlation exists, should be larger equal than 0
+		# as correlation is always as t_arr increases, T_arr does the same
+		print("At step {} pcc is: {} bit: {}".format(step, pcc, bit))
 
 		#analyze the goodness of the guess
-
+		if (step==0):
+			e_guess.append(1)
+		elif (abs(pcc[0]) > tau_corr):
+			e_guess.append(1)
+		else:
+			e_guess.append(0)
 
 		#update known private exponent and execute next step
-		c, s = me_step(c,s,public,n,step)
+		for test in messages:
+			test.c, test.s = me_step(test.c, test.s, int(bit), n)
+
 
 	c = mm(c,1,n,nb)
 	print(hex(c))
