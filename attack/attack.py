e8c0ab12f91455c508f30a1df0ca20a601ebc137
Giulio Roggero <roggero@eurecom.fr>
2019-05-03 14:41:23 +0200

    Only forward

--- attack/attack.py
+++ attack/attack.py
@@ -3,84 +3,8 @@ import os, sys
 import numpy
 import scipy.stats as stat
 import copy
+from msg_class import guess_test
 
-class guess_test():
-	def __init__(self,plain,T,n,nb):
-		self.plaintext = plain
-		self.n = n
-		self.nb = nb
-		self.T = T
-		self.t = 0
-		self.c = (1<<nb)%n
-		self.s = (plain*(1<<nb)) % n
-
-
-def mm(a,b,n,nb):
-	lt = [0, ~0]
-	res = 0
-	mask = 1
-	for i in range(nb):
-		ai = (a & mask) >> i
-		qi = (res + (ai&b)) & 1
-		res = (res + (lt[ai]&b) + (lt[qi]&n))
-
-		res = res >> 1
-		mask = mask << 1
-	return res
-
-def mm_estimate(a,b,n,nb):
-	lt = [0, ~0]
-	res = 0
-	mask = 1
-	estimate = 0
-	for i in range(nb):
-		ai = (a & mask) >> i
-		qi = (res + (ai&b)) & 1
-
-		estimate += (lt[ai]&1) + (lt[qi]&1)
-		res = (res + (lt[ai]&b) + (lt[qi]&n))
-
-		res = res >> 1
-		mask = mask << 1
-
-	return estimate
-
-def me_step(c, s, e_bit, n,nb):
-	if (e_bit):
-		c = mm(c,s,n,nb)
-	s = mm(s,s,n,nb)
-	return c, s
-
-def me_estimate(c, s, n, nb):
-	estimate = 0
-	estimate += mm_estimate(c,s,n,nb)
-	estimate += mm_estimate(s,s,n,nb)
-
-	return estimate
-
-def me(e,m,n,nb):
-# At some point we will settle up to a fixed number of bits
-# so the factor (2**(2nb) mod n) can actually be precomputed
-# once the public and the private key are decided  we still
-# need to find a way of compuing that
-#	c = mm(1, (2**(2nb) mod n),n,nb)
-#	s = mm(m, (2**(2nb) mod n),n,nb)
-#	c = mm(1,k0,n,nb)
-#	s = mm(m,k0,n,nb)
-	c = (1<<nb)%n
-	s = (m*(1<<nb)) % n
-	# print(padhex(c,160))
-	# print(padhex(s,160))
-	mask = 1
-	for x in range(nb):
-		if (e & mask):
-			c = mm(c,s,n,nb)
-		s = mm(s,s,n,nb)
-
-		mask = mask << 1
-		# print(str(x) + ' ' + padhex(c,160) + ' ' + padhex(s,160))
-	c = mm(c,1,n,nb)
-	return c
 
 def padbin(m, nb=128):
     return bin(m)[2:].zfill(nb)
@@ -103,10 +27,33 @@ def read_plain(n, nb=130, file_msg='PLAIN.BIN', file_time='TIME.BIN', length_msg
 	f_msg.close()
 	return messages, T_arr
 
+def step_forward(messages, bits_guessed, T_arr, tau):
 
+	t_arr = [msg.mm_estimate() for msg in messages]
+	pcc = stat.pearsonr(T_arr, t_arr)[0]
+	if (len(bits_guessed)==0):
+		# Being odd, LSB is 1, always
+		bits_guessed.append(1)
+	elif (pcc > tau):
+		# A correlation is observed, so one is guessed
+		bits_guessed.append(1)
+	else:
+		# else we append 0
+		bits_guessed.append(0)
 
-if __name__ == '__main__':
+	for msg in messages:
+		msg.me_step(bits_guessed[-1])
 
+	return pcc
+
+def step_back(messages, bits_guessed, pcc, count=1):
+
+	for msg in messages:
+		msg.revert(count)
+	del(bits_guessed[len(bits_guessed)-count:-1])
+	del(pcc[len(pcc)-count:-1])
+
+def main_attack():
 
 	#known informations
 	n       = 0x0c26e8d2105e3454baf122700611e915d
@@ -116,67 +63,25 @@ if __name__ == '__main__':
 	k0 	    = 0x08354f24c98cfac7a6ec8719a1b11ba4f
 	nb 		= 130
 
+	e_bits = list()
+
+
+	# Read messages from file
+	messages, T_arr = read_plain(n=n, nb=nb)
+
 	# Final to revert the key, as we start from LSB, just for testing with one bit at a time
 	private_key_bit = padbin(private)[::-1]
 	public_key_bit = padbin(public)[::-1]
 
-	# private exponent to be found
-	e_guess = list()
+	tau_th = 0.045
+	tau_low = 0.010
 
-	# control
-	step = 0
-	tau_corr = 0.045
-	tau_miss = 0.1
-
-	# c = (1<<nb)%n
-	# s = (m*(1<<nb)) % n
-	messages, T_arr = read_plain(n=n, nb=nb)
-
-	mcpy = copy.deepcopy(messages)
-
-
-	#messages[0] = guess_test(m,0,n,nb)
-
-	for step, bit in zip(range(nb), private_key_bit):
-		#calculate timing estimate based on guessing 1 for the current exponent bit
-		t_arr = list()
-		t_arr_wrong = list()
-		for test, trial in zip(messages, mcpy):
-			test.t = mm_estimate(test.c, test.s, n, nb)
-			# test.t = me_estimate(test.c, test.s, n, nb)
-			t_arr.append(test.t)
-			trial.t = mm_estimate(trial.c, trial.s, n, nb)
-			t_arr_wrong.append(trial.t)
-
-		# Calculate Pearson correlation coefficient
-		pcc = stat.pearsonr(T_arr, t_arr)
-		pcc_wrong = stat.pearsonr(T_arr, t_arr_wrong)
-		# Chiatant's Th : the PCC, if a correlation exists, should be larger equal than 0
-		# as correlation is always as t_arr increases, T_arr does the same
-
-		mcpy = copy.deepcopy(messages)
-
-		print("At step {:3} pcc is: {}  -- wrong {} bit:{}".format(step, pcc, pcc_wrong, bit))
-
-		#analyze the goodness of the guess
-		if (step==0):
-			e_guess.append(1)
-		elif (abs(pcc[0]) > tau_corr):
-			e_guess.append(1)
-		else:
-			e_guess.append(0)
-
-		#update known private exponent and execute next step
-		for test, trial in zip(messages, mcpy):
-			# test.c, test.s = me_step(test.c, test.s, e_guess[-1], n, nb)
-			test.c, test.s = me_step(test.c, test.s, int(bit,2), n, nb)
-			trial.c, trial.s = me_step(trial.c, trial.s, int(bit,2) ^ 1, n, nb)
+	for i in range(nb):
+		pcc = step_forward(messages, e_bits, T_arr, tau_th)
+		print("At step {} -- PCC: {:6} -- bit guessed: {} -- right {}".format(i,pcc,e_bits[-1],private_key_bit[i]))
 
 
 
 
-	# test if the step algorithm ran correctly
-	c = mm(messages[0].c,1,n,nb)
-	print(hex(c))
-	c = me(private,messages[0].plaintext,n,nb)
-	print(hex(c))
+if __name__ == '__main__':
+	main_attack()
--- attack/msg_class.py
+++ attack/msg_class.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import os, sys
-import copy
-#import numpy
-#import scipy.stats as stat
 
 class guess_test():
 	"""Class for handling the messages, with all the info related.
@@ -95,58 +91,3 @@ class guess_test():
 		self.t_me = self.hist[self.hist_len-1-count][4]
 		del(self.hist[self.hist_len-count:-1])
 		return self.hist[self.hist_len-1-count]
-
-
-
-def read_plain(n, nb=130, file_msg='PLAIN.BIN', file_time='TIME.BIN', length_msg=16, length_time=8):
-	### The number of bytes that composes the stuff to be read
-	f_msg = open(file_msg,"rb")
-	messages = list()
-	f_time = open(file_time, "rb")
-	T_arr = list()
-	count = 0
-	while True:
-		read_msg = f_msg.read(length_msg)
-		read_time = f_time.read(length_time)
-		if (not ((read_msg) and (read_time))) or (count > 5):
-			break
-		msg = guess_test(int.from_bytes(read_msg, byteorder='little',signed=False), int.from_bytes(read_time, byteorder='little',signed=False), n, nb)
-		T_arr.append(msg.T)
-		messages.append(msg)
-		count += 1
-	f_time.close()
-	f_msg.close()
-	return messages, T_arr
-
-
-
-if __name__ == '__main__':
-	n = 0x0c26e8d2105e3454baf122700611e915d
-	nb = 130
-	messages, T_arr = read_plain(n=n, nb=nb)
-	msgc = copy.deepcopy(messages)
-	meth_test = list()
-	func_test = list()
-	bit = 1
-	bitw = bit
-
-	for i in range(7):
-		for m, mw in zip(messages, msgc):
-			m.mm_estimate()
-			mw.mm_estimate()
-		if i == 7:
-			bitw = 0
-		for test, trial in zip(messages, msgc):
-			# test.c, test.s = me_step(test.c, test.s, e_guess[-1], n, nb)
-			test.me_step(bit)
-			trial.me_step(bitw)
-			#print("At {}: {} {} {} {}".format(i, test.c, trial.c, test.s, trial.s))
-
-	for m, mw in zip(messages, msgc):
-		#m.revert(2)
-		#mw.revert(2)
-		print(len(m.hist))
-		print("{} {} {} {}".format(m.c, mw.c, m.s, mw.s))
-
-		if ((m.c != mw.c) or (m.s != mw.s)):
-			print("Error: {} {} {} {}".format(m.c, mw.c, m.s, mw.s))
