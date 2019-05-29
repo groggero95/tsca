3a5a1a72f03080f5048bccaaec6e84b89d9ba192
Fausto Chiatante <fausto.chiatante@gmail.com>
2019-05-29 23:25:59 +0200

    corrected nasty overflow bug - god saves the pcc library

--- include/bigint.h
+++ include/bigint.h
@@ -8,7 +8,7 @@
 #include <string.h>
 
 //Following number has to be a multiple of 32
-#define INT_SIZE 512
+#define INT_SIZE 128
 #define VAR_SIZE 64
 #define NUMB_SIZE INT_SIZE/VAR_SIZE + 1
 #define HEX_DIGIT VAR_SIZE/4
--- include/panda4x4.h
+++ include/panda4x4.h
@@ -1,10 +1,10 @@
 #ifndef PANDA
 #define PANDA
 
-#define N_SAMPLES 		20000
-#define MSG_FILE 		"./data/P1M_Ofast_key4_512.BIN"
-#define TIME_FILE 		"./data/T1M_Ofast_key4_512.BIN"
-#define B_CONSIDERED 	3
+#define N_SAMPLES 		10000
+#define MSG_FILE 		"./data/P10k_Ofast_key0_128.BIN"
+#define TIME_FILE 		"./data/T10k_Ofast_key0_128.BIN"
+#define B_CONSIDERED 	2
 #define B_GUESSED 		1
 #define FILTERING		1
 
