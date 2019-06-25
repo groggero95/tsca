3c8665b85aa4b74e2c28d4aa7666a35fd1e93866
Giulio Roggero <roggero@eurecom.fr>
2019-06-25 15:49:34 +0200

    Few modifications

--- source/utils.c
+++ source/utils.c
@@ -1,8 +1,6 @@
-// MASTER-ONLY: DO NOT MODIFY THIS FILE
-
 /*
  * Copyright (C) Telecom ParisTech
- * 
+ *
  * This file must be used under the terms of the CeCILL. This source
  * file is licensed as described in the file COPYING, which you should
  * have received as part of this distribution. The terms are also
