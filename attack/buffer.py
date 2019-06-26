279923198ce2d5a1431e4fee462260a326f32554
Giulio Roggero <roggero@eurecom.fr>
2019-06-26 19:17:11 +0200

    Modified files

--- attack/buffer.py
+++ attack/buffer.py
@@ -1,3 +1,19 @@
+#
+# Copyright (C) EURECOM, Telecom Paris
+#
+# Prof. Renaud PACALET          <renaud.pacalet@telecom-paristech.fr>
+# Alberto ANSELMO               <Alberto.Anselmo@eurecom.fr>
+# Simone Alessandro CHIABERTO	<Simone-Alessandro.Chiaberto@eurecom.fr>
+# Fausto CHIATANTE              <Fausto.Chiatante@eurecom.fr>
+# Giulio ROGGERO                <Giulio.Roggero@eurecom.fr>
+#
+# This file must be used under the terms of the CeCILL. This source
+# file is licensed as described in the file COPYING, which you should
+# have received as part of this distribution. The terms are also
+# available at:
+# http://www.cecill.info/licences/Licence_CeCILL_V1.1-US.txt 
+#
+
 #!/usr/bin/python3
 def read_plain(n, nb=130, file_msg='PLAIN.BIN', file_time='TIME.BIN', length_msg=16, length_time=8):
 	### The number of bytes that composes the stuff to be read
--- test/mad.py
+++ test/mad.py
@@ -1,3 +1,19 @@
+#
+# Copyright (C) EURECOM, Telecom Paris
+#
+# Prof. Renaud PACALET          <renaud.pacalet@telecom-paristech.fr>
+# Alberto ANSELMO               <Alberto.Anselmo@eurecom.fr>
+# Simone Alessandro CHIABERTO	<Simone-Alessandro.Chiaberto@eurecom.fr>
+# Fausto CHIATANTE              <Fausto.Chiatante@eurecom.fr>
+# Giulio ROGGERO                <Giulio.Roggero@eurecom.fr>
+#
+# This file must be used under the terms of the CeCILL. This source
+# file is licensed as described in the file COPYING, which you should
+# have received as part of this distribution. The terms are also
+# available at:
+# http://www.cecill.info/licences/Licence_CeCILL_V1.1-US.txt 
+#
+
 #!/usr/bin/python3
 import os, sys
 import gmpy2
